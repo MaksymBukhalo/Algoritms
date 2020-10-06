c6dd1c5a3c0239b9a5f43eee11a69015eeb92c02
Maksim Bukhalo <maksimbukhalo1314@gmail.com>
2020-10-06 17:49:00 +0300

    intial comite

---
+++ Assets/Scripts/InstantiateAlgoritmsVisulisation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateAlgoritmsVisulisation : MonoBehaviour
+{
+	public GameObject ExampleVariable;
+	public RectTransform PanelViewContent;
+	public int ArraySize = 100;
+
+	private RectTransform exampleVeriableRectTransform;
+	private List<GameObject> variables;
+
+	private void Start()
+	{
+		exampleVeriableRectTransform = ExampleVariable.GetComponent<RectTransform>();
+	}
+
+	public void SetVariables()
+	{
+		Transform startPosition = ExampleVariable.transform;
+		float whidth = PanelViewContent.sizeDelta.x / ArraySize;
+		float Height = (PanelViewContent.sizeDelta.y / 100) * (Random.Range(0, 1000) / PanelViewContent.sizeDelta.y);
+		variables = new List<GameObject>(100);
+		for(int i = 0;i<variables.Count-1;i++)
+		{
+			
+		}
+	}
+
+	private RectTransform SetRectTransformNewVarieble(float width, float height)
+	{
+		RectTransform rectTransform = 
+	}
+}
